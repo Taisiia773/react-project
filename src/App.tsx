e14eda45fb7ee35df3a478baba88ecb35f803b86
Taisiia773 <110721426+Taisiia773@users.noreply.github.com>
2024-11-17 16:43:26 +0100

    3.1

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function AppComponent(){
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis unde quas possimus optio natus obcaecati magnam ab animi, molestiae nulla debitis, dolore doloribus aliquid dolores vitae quidem nesciunt veniam! Asperiores!</p>
+            <img src="https://i.obozrevatel.com/2018/4/20/kotochkikniga163261.jpg?size=1944x924" alt="" />
+        </div>
+    )
+}
